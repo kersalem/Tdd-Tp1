8e56a9e94790a67a7b83e2bfd4f04b9f55db907a
kersalem <kersalemarie@outlook.com>
2020-01-29 15:48:20 +0100

    testConcatenationId

--- tests/AdherentTest.php
+++ tests/AdherentTest.php
@@ -10,4 +10,12 @@ class AdherentTest extends TestCase
             $adherent
         );
     }
+
+    public function testConcatenationId() {
+        $adherent = new Adherent("PIERRE", "PAUL", new dateTime("1985-04-20"));
+        $this->assertEquals(
+            "PIERREPAUL20041985",
+            $adherent->getId()
+        );
+    }
 }
