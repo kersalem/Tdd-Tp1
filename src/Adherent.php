4fbd216c1585a915804cabb4acbe86f939416f72
Paulien <paulien.m.73@gmail.com>
2020-01-29 15:25:07 +0100

    create constructor

--- src/Adherent.php
+++ src/Adherent.php
@@ -3,5 +3,32 @@
 
 class Adherent
 {
+    /**
+     * @var string
+     */
+    private $nom;
 
+    /**
+     * @var string
+     */
+    private $prenom;
+
+    /**
+     * @var DateTime
+     */
+    private $dateDeNaissance;
+
+    /**
+     * Adherent constructor.
+     *
+     * @param string   $nom
+     * @param string   $prenom
+     * @param DateTime $dateDeNaissance
+     */
+    public function __construct(string $nom, string $prenom, DateTime $dateDeNaissance)
+    {
+        $this->nom             = $nom;
+        $this->prenom          = $prenom;
+        $this->dateDeNaissance = $dateDeNaissance;
+    }
 }
