8a861bb986e5c605c327b47acc11be71247e814c
kersalem <kersalemarie@outlook.com>
2020-01-29 15:20:47 +0100

    testCanBeCreated

--- tests/AdherentTest.php
+++ tests/AdherentTest.php
@@ -1,7 +1,13 @@
 <?php
+use PHPUnit\Framework\TestCase;
 
-
-class AdherentTest
+class AdherentTest extends TestCase
 {
-
+    public function testCanBeCreated() {
+        $adherent = new Adherent('Nom', 'Prenom', new DateTime());
+        $this->assertInstanceOf(
+            Adherent::class,
+            $adherent
+        );
+    }
 }
